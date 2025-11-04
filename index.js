c3f2412e3b95f42d668b1a73cc6f38bc6bc03e57
Guilherme Miranda <mirandax.gui13@gmail.com>
2025-11-04 17:23:09 -0300

    Form Solicitação Exames

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.use(bodyParser.json({ limit: '5mb' }));
 
 app.use('/', gerarPdfRoute);
 app.use('/', enviarKitsRoute);
+// Serve os arquivos estáticos da pasta 'build' do React
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Rota para servir o formulário React na raiz
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
