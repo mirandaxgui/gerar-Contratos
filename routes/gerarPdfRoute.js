7cd2b2921357ab19fa2f11aee4b07de86db8979c
Guilherme Miranda <mirandax.gui13@gmail.com>
2025-12-01 17:43:14 -0300

    INTRODUCING FARMER PROPOSTA

--- routes/gerarPdfRoute.js
+++ routes/gerarPdfRoute.js
@@ -15,10 +15,13 @@ const __dirname = dirname(__filename);
 
 const headerPath = path.resolve(__dirname, '../assets', 'header.png');
 const footerPath = path.resolve(__dirname, '../assets', 'footer.png');
+const logoDemaisPath = path.resolve(__dirname, '../assets', 'd+white-dark-bg.png');
 const imageMime = 'image/png';
 
 const imageBase64 = fs.readFileSync(headerPath).toString('base64');
 const imageFooterBase64 = fs.readFileSync(footerPath).toString('base64');
+const logoDemaisBase64 = fs.readFileSync(logoDemaisPath).toString('base64');
+
 
 
 
@@ -45,6 +48,10 @@ router.post('/gerar-pdf', async (req, res) => {
 
     const dataAtualExtenso = dataHojePtBrExtenso();
     const dataAtualCurta = dataHojePtBrCurta();
+    let tabelaProdutos = "";
+    let totalGeral = "";
+    let valorDesconto = "";
+    let valorFinal = "";
 
     // Se for propostaHunter, aplica a lógica de preços e injeta imagens
     let qtdColaboradores = Number(dados.campos?.qtd_colaboradores || 0);
@@ -55,11 +62,11 @@ router.post('/gerar-pdf', async (req, res) => {
         precos = obterPrecosPorVidas(qtdColaboradores);
       }
 
-      // 🖼️ Carrega e otimiza imagens pagina1..15 (exceto 13)
-      const pastaProposta = path.resolve(__dirname, '../assets/propostaHunter');
+      // 🖼️ PROPOSTA HUNTER - Carrega e otimiza imagens pagina1..15 (exceto 13)
+      const pastaPropostaHunter = path.resolve(__dirname, '../assets/propostaHunter');
       for (let i = 1; i <= 15; i++) {
         if (i === 13) continue;
-        const imagemPath = path.join(pastaProposta, `pagina${i}.png`);
+        const imagemPath = path.join(pastaPropostaHunter, `pagina${i}.png`);
         if (fs.existsSync(imagemPath)) {
           try {
             const buffer = await sharp(imagemPath)
@@ -76,6 +83,121 @@ router.post('/gerar-pdf', async (req, res) => {
         }
       }
     }
+
+    if (modelo === 'propostaFarmer') {
+
+      const produtos = dados.campos?.produtos || [];
+
+      totalGeral = dados.campos?.totalGeral;
+      valorDesconto = dados.campos?.valorDesconto;
+      valorFinal = dados.campos?.valorFinal;
+
+      tabelaProdutos = produtos.map(p => {
+        const custoTotal = Number(p.qtd) * Number(p.unit);
+        return `
+      <tr>
+          <td style="border:1px solid #193b33; padding:10px 12px;">
+              ${p.item}
+          </td>
+
+          <td style="border:1px solid #193b33; text-align:center; color:#00e08a;">
+              ${p.qtd}
+          </td>
+
+          <td style="border:1px solid #193b33; text-align:center; color:#00e08a;">
+              R$ ${Number(p.unit).toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
+          </td>
+
+          <td style="border:1px solid #193b33; text-align:center; color:#00e08a;">
+              R$ ${custoTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
+          </td>
+      </tr>
+    `;
+      }).join('\n');
+
+
+      // --------------------------------------------
+      // 🔹 LINHA DESCONTO (CONDICIONAL)
+      // --------------------------------------------
+      const linhaDesconto =
+        valorDesconto
+          ? `
+      <tr>
+        <td colspan="3"
+            style="border:1px solid #193b33; padding:10px 12px; text-align:center; font-weight:bold;">
+            Desconto para pagamento à vista
+        </td>
+        <td style="border:1px solid #193b33; text-align:center; font-weight:bold; color:#00e08a;">
+            ${valorDesconto}
+        </td>
+      </tr>`
+          : "";
+
+      // --------------------------------------------
+      // 🔹 LINHA PAGAMENTO (SEMPRE APARECE)
+      // --------------------------------------------
+      const linhaPagamento = `
+    <tr>
+      <td colspan="3"
+          style="border:1px solid #193b33; padding:10px 12px; text-align:center; font-weight:bold;">
+          Forma de Pagamento
+      </td>
+      <td style="border:1px solid #193b33; text-align:center; font-weight:bold; color:#00e08a;">
+          ${dados.campos.formaPagamento || ""}
+      </td>
+    </tr>
+    <tr>
+      <td colspan="3"
+          style="border:1px solid #193b33; padding:10px 12px; text-align:center; font-weight:bold;">
+          Quantidade de Parcelas
+      </td>
+      <td style="border:1px solid #193b33; text-align:center; font-weight:bold; color:#00e08a;">
+          ${dados.campos.parcelas ? ` ${dados.campos.parcelas}x` : ""}
+      </td>
+    </tr>
+     <tr>
+      <td colspan="3"
+          style="border:1px solid #193b33; padding:10px 12px; text-align:center; font-weight:bold;">
+          Data do Primeiro Pagamento
+      </td>
+      <td style="border:1px solid #193b33; text-align:center; font-weight:bold; color:#00e08a;">
+          ${dados.campos.dataPrimeiroPagamento ? ` ${dados.campos.dataPrimeiroPagamento}` : ""}
+      </td>
+    </tr>
+  `;
+
+      // 🔹 adiciona no objeto para o template
+      dados.campos.linhaDesconto = linhaDesconto;
+      dados.campos.linhaPagamento = linhaPagamento;
+
+
+      // Formatar valores finais
+      totalGeral = Number(totalGeral).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+      valorFinal = Number(valorFinal).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+
+      // CARREGAR IMAGENS
+      const pastaPropostaFarmer = path.resolve(__dirname, '../assets/propostaFarmer');
+      for (let i = 1; i <= 14; i++) {
+        if (i === 12) continue;
+        const imagemPath = path.join(pastaPropostaFarmer, `pagina${i}.png`);
+        if (fs.existsSync(imagemPath)) {
+          try {
+            const buffer = await sharp(imagemPath)
+              .jpeg({ quality: 80 })
+              .toBuffer();
+
+            const base64 = buffer.toString('base64');
+            dados.campos[`pagina${i}`] = `data:image/jpeg;base64,${base64}`;
+          } catch (err) {
+            console.warn(`⚠️ Erro ao processar ${imagemPath}:`, err.message);
+          }
+        } else {
+          console.warn(`⚠️ Imagem não encontrada: pagina${i}.png`);
+        }
+      }
+
+    }
+
     //LISTA DIRECIONADA AO TEMPLATE DE PROPOSTA DO PERIÓDICO
     const listaComplementares = (dados.campos?.complementaresAdc || '')
       .split(',')
@@ -89,14 +211,21 @@ router.post('/gerar-pdf', async (req, res) => {
       ...(dados.campos || {}),
       headerImage: `data:${imageMime};base64,${imageBase64}`,
       footerImage: `data:${imageMime};base64,${imageFooterBase64}`,
+      logoDemais: `data:${imageMime};base64,${logoDemaisBase64}`,
       dataAtualExtenso,
       dataAtual: dataAtualCurta,
+      //PROPOSTA HUNTER
       qtdColaboradores,
       precoBasico: precos.basico ? `R$ ${precos.basico},00` : '',
       precoEssencial: precos.essencial ? `R$ ${precos.essencial},00` : '',
       precoPremium: precos.premium ? `R$ ${precos.premium},00` : '',
       //LISTA DIRECIONADA AO TEMPLATE DE PROPOSTA DO PERIÓDICO
       complementaresAdcFormatado: listaComplementares,
+      //PROPOSTA FARMER
+      tabelaProdutos,
+      totalGeral,
+      valorDesconto,
+      valorFinal
 
     };
 
@@ -112,6 +241,17 @@ router.post('/gerar-pdf', async (req, res) => {
     await page.setContent(htmlFinal, { waitUntil: 'domcontentloaded', timeout: 0 });
     let pdfBuffer;
     if (dados.modelo === 'propostaHunter') {
+
+      pdfBuffer = await page.pdf({
+        width: '558mm',
+        height: '314mm',
+        printBackground: true,
+        margin: { top: 0, bottom: 0, left: 0, right: 0 }
+      });
+      await browser.close();
+    }
+
+    if (dados.modelo === 'propostaFarmer') {
 
       pdfBuffer = await page.pdf({
         width: '558mm',
@@ -144,6 +284,7 @@ router.post('/gerar-pdf', async (req, res) => {
       });
       await browser.close();
     };
+
 
 
     console.log(dados.enviarParaClicksign)
